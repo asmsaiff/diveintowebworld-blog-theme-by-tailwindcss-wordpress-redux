1e3ef1144ae1499185f82d730ebcfa5be0e22c3b
saidmsaif <saidmsaiff@gmail.com>
2021-07-31 20:57:38 +0600

    seperate post file

---
+++ template-parts/post-sections/cat-wordpress.php
@@ -0,0 +1,139 @@
+<section class="sm:pt-16 lg:pt-0 pb-8 gap-12 px-4 sm:px-0">
+    <h2 class="section-title text-2xl font-semibold pb-1">WordPress - All You Need to Know</h2>
+    <hr>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-10 mt-3 mb-6">
+        <div class="border-b sm:border-none pb-4 sm:pb-0">
+            <img src="https://preview.colorlib.com/theme/magdesign/images/ximg_2.jpg.pagespeed.ic.fbbBEgB1Q6.webp" alt="" class="rounded-lg">
+            
+            <div class="flex space-x-4 mt-6">
+                
+
+                <div class="flex text-sm font-semibold space-x-2">
+                    <h6 class="font-thin">Business, Travel</h6>
+                    <span>—</span>
+                    <p class="font-thin text-gray-500">7th July, 2021</p>
+                </div>
+            </div>
+
+            <h2 class="py-1 text-xl font-bold">Your most unhappy customers are your greatest source of learning.</h2>
+
+            <p class="mb-6 mt-2 text-gray-700">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam eum quisquam aspernatur iusto ratione ad inventore deserunt velit....
+            </p>
+
+            <div class="flex space-x-2">
+                <img src="https://preview.colorlib.com/theme/magdesign/images/xperson_1.jpg.pagespeed.ic.Zebptmx_f8.webp" alt="" class="rounded-full w-10 h-10">
+
+                <div>
+                    <h4 class="font-semibold">John Doe</h4>
+                    <p class="text-sm text-gray-500">CEO and Founder</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="border-b sm:border-none pb-4 sm:pb-0">
+            <img src="https://preview.colorlib.com/theme/magdesign/images/ximg_2.jpg.pagespeed.ic.fbbBEgB1Q6.webp" alt="" class="rounded-lg">
+            
+            <div class="flex space-x-4 mt-6">
+                
+
+                <div class="flex text-sm font-semibold space-x-2">
+                    <h6 class="font-thin">Business, Travel</h6>
+                    <span>—</span>
+                    <p class="font-thin text-gray-500">7th July, 2021</p>
+                </div>
+            </div>
+
+            <h2 class="py-1 text-xl font-bold">Your most unhappy customers are your greatest source of learning.</h2>
+
+            <p class="mb-6 mt-2 text-gray-700">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam eum quisquam aspernatur iusto ratione ad inventore deserunt velit....
+            </p>
+
+            <div class="flex space-x-2">
+                <img src="https://preview.colorlib.com/theme/magdesign/images/xperson_1.jpg.pagespeed.ic.Zebptmx_f8.webp" alt="" class="rounded-full w-10 h-10">
+
+                <div>
+                    <h4 class="font-semibold">John Doe</h4>
+                    <p class="text-sm text-gray-500">CEO and Founder</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="border-b sm:border-none pb-4 sm:pb-0">
+            <img src="https://preview.colorlib.com/theme/magdesign/images/ximg_3.jpg.pagespeed.ic.TsSrvxpHvJ.webp" alt="" class="rounded-lg">
+            
+            <div class="flex space-x-4 mt-6">
+                <div class="flex text-sm font-semibold space-x-2">
+                    <h6 class="font-thin">Business, Travel</h6>
+                    <span>—</span>
+                    <p class="font-thin text-gray-500">7th July, 2021</p>
+                </div>
+            </div>
+
+            <h2 class="py-1 text-xl font-bold">Your most unhappy customers are your greatest source of learning.</h2>
+
+            <p class="mb-6 mt-2 text-gray-700">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam eum quisquam aspernatur iusto ratione ad inventore deserunt velit....
+            </p>
+
+            <div class="flex space-x-2">
+                <img src="https://preview.colorlib.com/theme/magdesign/images/xperson_1.jpg.pagespeed.ic.Zebptmx_f8.webp" alt="" class="rounded-full w-10 h-10">
+
+                <div>
+                    <h4 class="font-semibold">John Doe</h4>
+                    <p class="text-sm text-gray-500">CEO and Founder</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-10 border-b sm:border-none pb-4 sm:pb-0">
+        <div class="border-b sm:border-none pb-4 sm:pb-0">
+            <div class="flex space-x-4 md:mt-6">
+                <div class="flex text-sm font-semibold space-x-2">
+                    <h6 class="font-thin">Business, Travel</h6>
+                    <span>—</span>
+                    <p class="font-thin text-gray-500">7th July, 2021</p>
+                </div>
+            </div>
+
+            <h2 class="py-1 text-xl font-bold">Your most unhappy customers are your greatest source of learning.</h2>
+
+            <p class="md:mb-6 mt-2 text-gray-700">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam eum quisquam aspernatur iusto ratione ad inventore deserunt velit....
+            </p>
+        </div>
+
+        <div class="border-b sm:border-none pb-4 sm:pb-0">
+            <div class="flex space-x-4 md:mt-6">
+                <div class="flex text-sm font-semibold space-x-2">
+                    <h6 class="font-thin">Business, Travel</h6>
+                    <span>—</span>
+                    <p class="font-thin text-gray-500">7th July, 2021</p>
+                </div>
+            </div>
+
+            <h2 class="py-1 text-xl font-bold">Your most unhappy customers are your greatest source of learning.</h2>
+
+            <p class="md:mb-6 mt-2 text-gray-700">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam eum quisquam aspernatur iusto ratione ad inventore deserunt velit....
+            </p>
+        </div>
+        
+        <div class="border-b sm:border-none pb-4 sm:pb-0">
+            <div class="flex space-x-4 md:mt-6">
+                <div class="flex text-sm font-semibold space-x-2">
+                    <h6 class="font-thin">Business, Travel</h6>
+                    <span>—</span>
+                    <p class="font-thin text-gray-500">7th July, 2021</p>
+                </div>
+            </div>
+
+            <h2 class="py-1 text-xl font-bold">Your most unhappy customers are your greatest source of learning.</h2>
+
+            <p class="md:mb-6 mt-2 text-gray-700">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam eum quisquam aspernatur iusto ratione ad inventore deserunt velit....
+            </p>
+        </div>
+    </div>
+</section>
---
+++ template-parts/post-sections/modern-web.php
@@ -0,0 +1,157 @@
+<section class="pt-6 sm:pt-16 pb-8 grid xl:grid-cols-2 gap-12 px-4 sm:px-0">
+    <div>
+        <h2 class="section-title text-2xl font-semibold pb-1">Modern Web Technologies</h2>
+        <hr>
+        <div class="grid grid-cols-1 gap-6 mt-3">
+            <div class="flex flex-col sm:flex-row sm:space-x-6 border-b sm:border-none pb-4 sm:pb-0">
+                <img src="https://preview.colorlib.com/theme/magdesign/images/ximg_4.jpg.pagespeed.ic.2DwdgZu3vw.webp" alt="" class="rounded-lg w-full pb-3 sm:pb-0 sm:w-36 md:w-56 h-auto self-center">
+                
+                <div>
+                    <div class="flex space-x-4">
+                        <div class="flex text-sm font-semibold space-x-2">
+                            <h6 class="font-thin">Business, Travel</h6>
+                            <span>—</span>
+                            <p class="font-thin text-gray-500">7th July, 2021</p>
+                        </div>
+                    </div>
+    
+                    <h2 class="py-1 text-xl font-bold">Your most unhappy customers are your greatest source of learning.</h2>
+    
+                    <p class="text-gray-700 text-sm">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam eum quisquam aspernatur iusto ratione ad inventore deserunt velit....
+                    </p>
+                </div>
+            </div>
+            
+            <div class="flex sm:space-x-6 border-b sm:border-none pb-4 sm:pb-0">
+                <img src="https://preview.colorlib.com/theme/magdesign/images/ximg_4.jpg.pagespeed.ic.2DwdgZu3vw.webp" alt="" class="rounded-lg w-56 h-auto self-center hidden sm:block sm:w-36 md:w-56">
+                
+                <div>
+                    <div class="flex space-x-4">
+                        <div class="flex text-sm font-semibold space-x-2">
+                            <h6 class="font-thin">Business, Travel</h6>
+                            <span>—</span>
+                            <p class="font-thin text-gray-500">7th July, 2021</p>
+                        </div>
+                    </div>
+    
+                    <h2 class="py-1 text-xl font-bold">Your most unhappy customers are your greatest source of learning.</h2>
+    
+                    <p class="text-gray-700 text-sm">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam eum quisquam aspernatur iusto ratione ad inventore deserunt velit....
+                    </p>
+                </div>
+            </div>
+
+            <div class="flex sm:space-x-6 border-b sm:border-none pb-4 sm:pb-0">
+                <img src="https://preview.colorlib.com/theme/magdesign/images/ximg_4.jpg.pagespeed.ic.2DwdgZu3vw.webp" alt="" class="rounded-lg w-56 h-auto self-center hidden sm:block sm:w-36 md:w-56">
+                
+                <div>
+                    <div class="flex space-x-4">
+                        <div class="flex text-sm font-semibold space-x-2">
+                            <h6 class="font-thin">Business, Travel</h6>
+                            <span>—</span>
+                            <p class="font-thin text-gray-500">7th July, 2021</p>
+                        </div>
+                    </div>
+    
+                    <h2 class="py-1 text-xl font-bold">Your most unhappy customers are your greatest source of learning.</h2>
+    
+                    <p class="text-gray-700 text-sm">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam eum quisquam aspernatur iusto ratione ad inventore deserunt velit....
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <h2 class="section-title text-2xl font-semibold pb-1">Development Roadmap</h2>
+        <hr>
+        <div class="mt-4 mb-8">
+            <div class="flex flex-col sm:flex-row sm:space-x-6 border-b sm:border-none pb-4 sm:pb-0">
+                <img src="https://preview.colorlib.com/theme/magdesign/images/ximg_4.jpg.pagespeed.ic.2DwdgZu3vw.webp" alt="" class="rounded-lg sm:w-56 h-auto self-center mb-4 sm:mb-0">
+                
+                <div>
+                    <div class="flex space-x-4">
+                        <div class="flex text-sm font-semibold space-x-2">
+                            <h6 class="font-thin">Business, Travel</h6>
+                            <span>—</span>
+                            <p class="font-thin text-gray-500">7th July, 2021</p>
+                        </div>
+                    </div>
+    
+                    <h2 class="py-1 text-xl font-bold">Your most unhappy customers are your greatest source of learning.</h2>
+    
+                    <p class="text-gray-700 text-sm">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam eum quisquam aspernatur iusto ratione ad inventore deserunt velit....
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="grid sm:grid-cols-2 gap-10">
+            <div class="border-b sm:border-none pb-4 sm:pb-0">
+                <div class="flex space-x-4">
+                    <div class="flex text-sm font-semibold space-x-2">
+                        <h6 class="font-thin">Business, Travel</h6>
+                        <span>—</span>
+                        <p class="font-thin text-gray-500">7th July, 2021</p>
+                    </div>
+                </div>
+
+                <h2 class="py-1 text-lg font-bold">Your most unhappy customers are your greatest source of learning.</h2>
+
+                <p class="text-gray-700 text-sm">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam eum quisquam aspernatur iusto ratione ad inventore....
+                </p>
+            </div>
+
+            <div class="border-b sm:border-none pb-4 sm:pb-0">
+                <div class="flex space-x-4">
+                    <div class="flex text-sm font-semibold space-x-2">
+                        <h6 class="font-thin">Business, Travel</h6>
+                        <span>—</span>
+                        <p class="font-thin text-gray-500">7th July, 2021</p>
+                    </div>
+                </div>
+
+                <h2 class="py-1 text-lg font-bold">Your most unhappy customers are your greatest source of learning.</h2>
+
+                <p class="text-gray-700 text-sm">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam eum quisquam aspernatur iusto ratione ad inventore....
+                </p>
+            </div>
+
+            <div class="border-b sm:border-none pb-4 sm:pb-0">
+                <div class="flex space-x-4">
+                    <div class="flex text-sm font-semibold space-x-2">
+                        <h6 class="font-thin">Business, Travel</h6>
+                        <span>—</span>
+                        <p class="font-thin text-gray-500">7th July, 2021</p>
+                    </div>
+                </div>
+
+                <h2 class="py-1 text-lg font-bold">Your most unhappy customers are your greatest source of learning.</h2>
+
+                <p class="text-gray-700 text-sm">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam eum quisquam aspernatur iusto ratione ad inventore....
+                </p>
+            </div>
+
+            <div class="border-b sm:border-none pb-4 sm:pb-0">
+                <div class="flex space-x-4">
+                    <div class="flex text-sm font-semibold space-x-2">
+                        <h6 class="font-thin">Business, Travel</h6>
+                        <span>—</span>
+                        <p class="font-thin text-gray-500">7th July, 2021</p>
+                    </div>
+                </div>
+
+                <h2 class="py-1 text-lg font-bold">Your most unhappy customers are your greatest source of learning.</h2>
+
+                <p class="text-gray-700 text-sm">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam eum quisquam aspernatur iusto ratione ad inventore....
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
