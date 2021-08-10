4aeeb08b3728db55285e3ed40415e97e815c13f9
saidmsaif <saidmsaiff@gmail.com>
2021-08-10 09:56:31 +0600

    CPT ui plugin removed

--- inc/diveintowebworld-plugins.php
+++ inc/diveintowebworld-plugins.php
@@ -69,14 +69,14 @@ function diveintowebworld_register_required_plugins() {
 			'force_deactivation' => false,
 		),
 
-		array(
-			'name'               => 'Custom Post Type UI',
-			'slug'               => 'custom-post-type-ui',
-			'required'           => true,
-			'version'            => '1.9.2',
-			'force_activation'   => true,
-			'force_deactivation' => false,
-		),
+		// array(
+		// 	'name'               => 'Custom Post Type UI',
+		// 	'slug'               => 'custom-post-type-ui',
+		// 	'required'           => true,
+		// 	'version'            => '1.9.2',
+		// 	'force_activation'   => true,
+		// 	'force_deactivation' => false,
+		// ),
 	);
 
 	/*
